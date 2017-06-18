385e0b460f3fb9234e3a3b1c136abe610bcc5b59
LukaKerr <lukakerr1@gmail.com>
2017-06-18 17:55:27 +1000

    Initial commit

---
+++ Tray-App-Template/AppDelegate.swift
@@ -0,0 +1,52 @@
+//
+//  AppDelegate.swift
+//  Tray-App-Template
+//
+//  Created by Luka Kerr on 2/4/17.
+//  Copyright © 2017 Luka Kerr. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+	
+	@IBOutlet weak var window: NSWindow!
+	
+	let popover = NSPopover()
+	let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+	
+	func showPopover(sender: AnyObject?) {
+		NSApplication.shared.activate(ignoringOtherApps: true)
+		if let button = statusItem.button {
+			popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+		}
+	}
+ 
+	func closePopover(sender: AnyObject?) {
+		popover.performClose(sender)
+	}
+ 
+	@objc func togglePopover(sender: AnyObject?) {
+		if popover.isShown {
+			closePopover(sender: sender)
+		} else {
+			showPopover(sender: sender)
+		}
+	}
+	
+	func applicationDidFinishLaunching(_ notification: Notification) {
+		if let button = statusItem.button {
+			button.image = NSImage(named: NSImage.Name(rawValue: "icon"))
+			button.target = self
+			button.action = #selector(self.togglePopover(sender:))
+		}
+		popover.contentViewController = MenuViewController(nibName: NSNib.Name(rawValue: "MenuViewController"), bundle: nil)
+		// Disable delay for popover - default is true
+		popover.animates = false
+		// When something else is clicked, close the popover
+		popover.behavior = .transient
+	}
+	
+	
+}
---
+++ Tray-App-Template/Popover.swift
@@ -0,0 +1,33 @@
+//
+//  Popover.swift
+//  Tray-App-Template
+//
+//  Created by Luka Kerr on 18/6/17.
+//  Copyright © 2017 Luka Kerr. All rights reserved.
+//
+
+import Cocoa
+
+class Popover: NSView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+    }
+	
+	override func viewDidMoveToWindow() {
+		
+		guard let frameView = window?.contentView?.superview else {
+			return
+		}
+		
+		// Set custom colour for the view - in this case the popover
+		let backgroundView = NSView(frame: frameView.bounds)
+		backgroundView.wantsLayer = true
+		backgroundView.layer?.backgroundColor = NSColor(red: 0.21, green: 0.24, blue: 0.28, alpha: 1).cgColor
+		backgroundView.autoresizingMask = [.width, .height]
+		
+		frameView.addSubview(backgroundView, positioned: .below, relativeTo: frameView)
+		
+	}
+    
+}
