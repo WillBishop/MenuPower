f2d12c3d52e5b95b311d81afc6ba65fb82b50a5c
arne-me-com <arne@me.com>
2017-11-23 12:17:57 +0100

    Update AppDelegate.swift
    Enable Dark Mode icon

--- Tray-App-Template/AppDelegate.swift
+++ Tray-App-Template/AppDelegate.swift
@@ -38,6 +38,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 	func applicationDidFinishLaunching(_ notification: Notification) {
 		if let button = statusItem.button {
 			button.image = NSImage(named: NSImage.Name(rawValue: "icon"))
+			button.image?.isTemplate = true //enable Dark Mode icon 
 			button.target = self
 			button.action = #selector(self.togglePopover(sender:))
 		}
